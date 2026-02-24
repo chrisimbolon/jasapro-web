309aed30a50f69b83de125c3403f4f433f453284
Christyan Simbolon <sambungdata@gmail.com>
2026-02-24 21:04:51 +0700

    chore: setup i18n

---
+++ src/lib/i18n.ts
@@ -0,0 +1,27 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import enCommon from "../locales/en/common";
+import idCommon from "../locales/id/common";
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        common: enCommon,
+      },
+      id: {
+        common: idCommon,
+      },
+    },
+    fallbackLng: "en",
+    defaultNS: "common",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
